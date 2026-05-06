e2729d830fb69c2ceb90feb6f48ab1706c4ba49a
sness88 <sness88@gmail.com>
2026-05-06 19:56:33 -0400

    Add files via upload

--- functions/create-link-token.js
+++ functions/create-link-token.js
@@ -10,7 +10,7 @@ exports.handler = async (event) => {
   }
 
   try {
-    const response = await fetch('https://production.plaid.com/link/token/create', {
+    const response = await fetch('https://development.plaid.com/link/token/create', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
